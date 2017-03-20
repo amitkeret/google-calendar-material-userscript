423bb58b26b099cb0f82942caf6914ada5a84172
Amit Keret <amitkeretwork@gmail.com>
2017-03-20 22:19:28 +1000

    gmaps-api initial commit 1. place photos for addresses with place matches 2. static maps for other addresses

--- calendarMaterial.js
+++ calendarMaterial.js
@@ -7,9 +7,13 @@
 // @updateURL    https://raw.githubusercontent.com/amitkeret/google-calendar-material-userscript/master/calendarMaterial.js
 // @resource     calendarCSS       https://raw.githubusercontent.com/amitkeret/google-calendar-material-userscript/master/calendarMaterial.css
 // @resource     calendarFlairData https://raw.githubusercontent.com/amitkeret/google-calendar-material-userscript/master/calendarFlairData.json
+// @require      https://maps.google.com/maps/api/js?libraries=places
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
+
+// expose this for subsequent google api scripts
+unsafeWindow.google = google;
 
 (function() {
     'use strict';
@@ -18,6 +22,12 @@
 
     GM_addStyle(GM_getResourceText('calendarCSS'));
     const calendarFlairData = JSON.parse(GM_getResourceText('calendarFlairData')).calendarFlairData;
+
+    const GMaps = {
+      'dimentions': { 'width': 2002, 'height': 696 },
+      'geocoder':   new google.maps.Geocoder(),
+      'places':     new google.maps.places.PlacesService(document.createElement('div')) // DOM elemenet param required
+    };
 
     function globalchanges() {
         let goog = document.querySelector('.gb_3b .gb_5b'),
@@ -41,6 +51,13 @@
         return baseImgURL + '.jpg';
     }
 
+    function getStaticMapURL(address) {
+        let prefix = 'https://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&format=png&visual_refresh=true',
+            size = '&size=' + GMaps.dimentions.width + 'x' + GMaps.dimentions.height + '&scale=2',
+            add = '&markers=color:red|size:small|' + encodeURIComponent(address);
+        return prefix + size + add;
+    }
+
     function checkForFlair(text, element) {
         let flair = false,
             exactFunction = function(el) { return text.toLowerCase() == el; },
@@ -60,6 +77,36 @@
             }
         }
         if (false !== flair) applyBg(element, getBgURL('flair', flair));
+    }
+
+    function checkForGeo(address, element) {
+        GMaps.geocoder.geocode({ 'address': address }, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
+                applyBg(element, getStaticMapURL(address));
+            }
+        });
+    }
+
+    function checkForPlace(address, element) {
+        GMaps.places.textSearch({ 'query': address }, function (results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK && results.length > 0) {
+                let placeId = results[0].place_id;
+                GMaps.places.getDetails({ 'placeId' : placeId}, function (place, status) {
+                    if (status == google.maps.places.PlacesServiceStatus.OK && place.photos && place.photos.length > 0) {
+                        element.classList.add('fadein');
+                        let placeURL = place.photos[0].getUrl({'maxWidth': GMaps.dimentions.width, 'maxHeight': GMaps.dimentions.height});
+                        applyBg(element, placeURL);
+                    }
+                    else checkForGeo(address, element);
+                });
+            }
+            else checkForGeo(address, element);
+        });
+    }
+
+    function checkForAddress(address, element, checkPlace) {
+        if (false === checkPlace) checkForGeo(address, element);
+        else checkForPlace(address, element);
     }
 
     function changeGlobalBg() {
@@ -86,6 +133,11 @@
                     span2.innerText = eventTime;
                     eventTitle.appendChild(span2);
                 }
+                /**
+                 * Doesn't work - possibly too many calls on Mutation call?
+                 */
+                //let addressLink = eventTitle.querySelector('.lv-location');
+                //if (addressLink) checkForAddress(addressLink.innerText, eventTitle, false);
                 checkForFlair(eventTitle.innerText, eventTitle);
                 event.dataset.styled = 1;
             });
@@ -98,6 +150,12 @@
             var bgimg = document.createElement('div');
             bgimg.id = 'ep-bgimg';
             event.appendChild(bgimg);
+            let addressLink = event.querySelector('.ep-dp-input');
+            if (addressLink) {
+                let addressLinkInput = addressLink.querySelector('input');
+                if (addressLinkInput && addressLinkInput.value.length > 0) checkForAddress(addressLinkInput.value, bgimg);
+                else if (addressLink.innerText.length > 0) checkForAddress(addressLink.innerText, bgimg);
+            }
             let epTitle = document.querySelector('.ep-title');
             if (epTitle.querySelectorAll('input').length) checkForFlair(epTitle.querySelector('input').value, bgimg);
             else checkForFlair(epTitle.innerText, bgimg);
@@ -120,6 +178,9 @@
             let bgimg = document.createElement('div');
             bgimg.id = 'bubble-bgimg';
             bubbleEvent.insertBefore(bgimg, bubbleEvent.firstChild);
+            let addressLink = bubbleEvent.querySelector('a.neb-map-link');
+            // .innerText include "map" at the end of the string, have to slice() it out...
+            if (addressLink) checkForAddress(addressLink.parentNode.innerText.split('').slice(0, -3).join(''), bgimg);
             checkForFlair(bubbleEvent.innerText.split('\n')[0].trim(), bgimg);
         }, 1);
     }
