9cd60550145dc2b1d6567693d27a563f8829005a
Amit Keret <amitkeretwork@gmail.com>
2017-11-17 17:59:44 +1000

    version bump

--- calendarMaterial.js
+++ calendarMaterial.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google calendar Material design
-// @version      0.4.0
+// @version      0.4.1
 // @description
 // @author       amitkeret
 // @match        https://calendar.google.com/calendar/*
